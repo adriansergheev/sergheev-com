255775bf375ca526e5a474a46ec1d4f5197b7255
Adrian Sergheev <sergheevdev@icloud.com>
2024-05-17 14:23:07 +0200

    wip

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,14 +2,6 @@ import Vapor
 
 func routes(_ app: Application) throws {
     app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-    
-    app.get("docker") { req async -> String in
-        "Hello, docker!"
+        req.fileio.streamFile(at: "Public/index.html") 
     }
 }
