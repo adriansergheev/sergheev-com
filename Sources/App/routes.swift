009bffcd4713bc20571046c4d93cd76420fd82a0
Adrian Sergheev <sergheevdev@icloud.com>
2024-05-19 11:27:16 +0200

    wip

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -4,4 +4,7 @@ func routes(_ app: Application) throws {
     app.get { req async in
         req.fileio.streamFile(at: "Public/index.html") 
     }
+    app.get("photoguessr-appstore") { req async in
+    req.fileio.streamFile(at: "Public/photoguessr-appstore.html")
+    }
 }
