326b2349bc7c25760b98d5092b1773e5e53a2654
Adrian Sergheev <sergheevdev@icloud.com>
2024-05-23 11:57:44 +0200

    add dependency

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,13 +1,14 @@
 import Vapor
+import HtmlVaporSupport
 
-func routes(_ app: Application) throws {
-    app.get { req async in
-        req.fileio.streamFile(at: "Public/index.html") 
-    }
-    app.get("photoguessr-appstore") { req async in
-        req.fileio.streamFile(at: "Public/photoguessr-appstore.html")
-    }
-    app.get("docker") { req in
-        "docker" 
-    }
+func routes(_ app: Vapor.Application) throws {
+	app.get { req async in
+		req.fileio.streamFile(at: "Public/index.html")
+	}
+	//	app.get { req async in
+	//		return Node.h1("Hello, type-safe HTML on Vapor!")
+	//	}
+	app.get("photoguessr-appstore") { req async in
+		req.fileio.streamFile(at: "Public/photoguessr-appstore.html")
+	}
 }
