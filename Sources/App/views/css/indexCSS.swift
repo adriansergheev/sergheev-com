2a669a5da4853330ca63bec86688418d77fda95e
Adrian Sergheev <sergheevdev@icloud.com>
2024-09-22 11:59:15 +0200

    new home (#6)

--- Sources/App/views/css/indexCSS.swift
+++ Sources/App/views/css/indexCSS.swift
@@ -1,44 +1,49 @@
 let indexCSS: StaticString = """
- body, html {
- height: 100%;
- margin: 0;
- display: flex;
- justify-content: center;
- align-items: center;
- background-color: #000;
- font-family: Arial, sans-serif;
- color: #e1e8d0;
- }
- .box {
- background-color: #000;
- color: #e1e8d0;
- border: 1px solid #e1e8d0;
- padding: 40px;
- border-radius: 10px;
- text-align: center;
- width: 300px;
- }
- .box h1 {
- margin: 0;
- font-size: 24px;
- }
- .divider {
- height: 1px;
- background-color: #e1e8d0;
- margin: 20px 0;
- }
- .box p {
- margin: 10px 0 20px;
- }
- .box a {
- display: inline-block;
- padding: 10px 20px;
- border: 1px solid #e1e8d0;
- border-radius: 5px;
- text-decoration: none;
- color: #e1e8d0;
- }
- .link {
- margin-right: 10px;
- }
+        body,
+        html {
+            height: 100%;
+            margin: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-color: #000;
+            font-family: Arial, sans-serif;
+            color: #e1e8d0;
+        }
+
+        .box {
+            background-color: #000;
+            color: #e1e8d0;
+            width: 600px;
+        }
+
+        .box h1 {
+            margin: 0;
+            font-size: 24px;
+        }
+
+        .divider {
+            height: 1px;
+            background-color: #e1e8d0;
+            margin: 20px 0;
+        }
+
+        .box p {
+            margin: 10px 0 20px;
+            line-height: 1.5;
+        }
+
+        .box a {
+            display: block;
+            padding: 6px 0px;
+            text-decoration: none;
+            color: #e1e8d0;
+        }
+
+        .box a.inline-link {
+            display: inline;
+            padding: 0;
+            color: inherit;
+            text-decoration: underline;
+        }
 """
