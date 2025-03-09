dc4f3a06dd846ad75d008a10f0cc86084a61119c
Adrian Sergheev <sergheevdev@icloud.com>
2025-03-09 17:38:50 +0200

    use vapor routing

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,23 +1,39 @@
 import Vapor
-import HtmlVaporSupport
+@preconcurrency import VaporRouting
 
-// configures your application
 public func configure(_ app: Vapor.Application) async throws {
-	app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-	// register routes
-	try routes(app)
+  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  try routes(app)
 }
 
 func routes(_ app: Vapor.Application) throws {
-	app.get { req async in
-		layout(title: "home", content: homePage)
-	}
-	app.get("photoguessr-appstore") { req async in
-		req.fileio.streamFile(at: "Public/photoguessr-appstore.html")
-	}
-	//	app.get("images", ":imageName") { req async in
-	//		guard let imageName = req.parameters.get("imageName")
-	//		else { return Response.init(status: .notFound) }
-	//		return req.fileio.streamFile(at: app.directory.publicDirectory + "images/" + imageName)
-	//	}
+  app.mount(router, use: siteHandler)
 }
+
+let router = OneOf {
+  Route(.case(SiteRoute.home))
+  Route(.case(SiteRoute.photoGuessrAppStore))
+}
+
+enum SiteRoute {
+  case home
+  case photoGuessrAppStore
+}
+
+func siteHandler(
+  request: Request,
+  route: SiteRoute
+) async throws -> AsyncResponseEncodable {
+  switch route {
+  case .home:
+    return layout(title: "home", content: homePage)
+  case .photoGuessrAppStore:
+    return request.fileio.streamFile(at: "Public/photoguessr-appstore.html")
+  }
+  //  app.get("images", ":imageName") { req async in
+  //    guard let imageName = req.parameters.get("imageName")
+  //    else { return Response.init(status: .notFound) }
+  //    return req.fileio.streamFile(at: app.directory.publicDirectory + "images/" + imageName)
+  //  }
+}
+
