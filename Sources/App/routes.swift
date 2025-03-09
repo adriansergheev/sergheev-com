908362e3f4a126c13d904e8e1fd1bbbb1629d9f6
Adrian Sergheev <sergheevdev@icloud.com>
2025-03-09 18:08:41 +0200

    Revert "use vapor routing (#7)"
    This reverts commit 6ce5cd448e52a3f23af95ccb558bd185a6d570c1.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,39 +1,23 @@
 import Vapor
-@preconcurrency import VaporRouting
+import HtmlVaporSupport
 
+// configures your application
 public func configure(_ app: Vapor.Application) async throws {
-  app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-  try routes(app)
+	app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+	// register routes
+	try routes(app)
 }
 
 func routes(_ app: Vapor.Application) throws {
-  app.mount(router, use: siteHandler)
+	app.get { req async in
+		layout(title: "home", content: homePage)
+	}
+	app.get("photoguessr-appstore") { req async in
+		req.fileio.streamFile(at: "Public/photoguessr-appstore.html")
+	}
+	//	app.get("images", ":imageName") { req async in
+	//		guard let imageName = req.parameters.get("imageName")
+	//		else { return Response.init(status: .notFound) }
+	//		return req.fileio.streamFile(at: app.directory.publicDirectory + "images/" + imageName)
+	//	}
 }
-
-let router = OneOf {
-  Route(.case(SiteRoute.home))
-  Route(.case(SiteRoute.photoGuessrAppStore))
-}
-
-enum SiteRoute {
-  case home
-  case photoGuessrAppStore
-}
-
-func siteHandler(
-  request: Request,
-  route: SiteRoute
-) async throws -> AsyncResponseEncodable {
-  switch route {
-  case .home:
-    return layout(title: "home", content: homePage)
-  case .photoGuessrAppStore:
-    return request.fileio.streamFile(at: "Public/photoguessr-appstore.html")
-  }
-  //  app.get("images", ":imageName") { req async in
-  //    guard let imageName = req.parameters.get("imageName")
-  //    else { return Response.init(status: .notFound) }
-  //    return req.fileio.streamFile(at: app.directory.publicDirectory + "images/" + imageName)
-  //  }
-}
-
