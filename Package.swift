627d73c4fbdce85591bfcd8a0f9cb8d4dc2860d4
Adrian Sergheev <sergheevdev@icloud.com>
2025-03-11 11:45:42 +0100

    Revert "vapor-routing-second-attempt (#8)"
    This reverts commit 89925e9d31958cc80042aa40344f0634577fd9d4.

--- Package.swift
+++ Package.swift
@@ -2,49 +2,47 @@
 import PackageDescription
 
 let package = Package(
-  name: "sergheev",
-  platforms: [
-    .macOS(.v13),
-  ],
-  products: [
-    .library(name: "App", targets: ["App"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
-    .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
-    .package(url: "https://github.com/pointfreeco/swift-html-vapor", from: "0.5.0"),
-    .package(url: "https://github.com/pointfreeco/vapor-routing", from: "0.1.3")
-  ],
-  targets: [
-    .executableTarget(
-      name: "Run",
-      dependencies: [
-        "App"
-      ]
-    ),
-    .target(
-      name: "App",
-      dependencies: [
-        .product(name: "Vapor", package: "vapor"),
-        .product(name: "NIOCore", package: "swift-nio"),
-        .product(name: "NIOPosix", package: "swift-nio"),
-        .product(name: "HtmlVaporSupport", package: "swift-html-vapor"),
-        .product(name: "VaporRouting", package: "vapor-routing")
-      ],
-      swiftSettings: swiftSettings
-    ),
-    .testTarget(
-      name: "AppTests",
-      dependencies: [
-        .target(name: "App"),
-        .product(name: "XCTVapor", package: "vapor"),
-      ],
-      swiftSettings: swiftSettings
-    )
-  ]
+	name: "sergheev",
+	platforms: [
+		.macOS(.v13)
+	],
+	products: [
+		.library(name: "App", targets: ["App"]),
+	],
+	dependencies: [
+		.package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
+		.package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+		.package(url: "https://github.com/adriansergheev/swift-html-vapor.git", revision: "f0eb9d7")
+	],
+	targets: [
+		.executableTarget(
+			name: "Run",
+			dependencies: [
+				"App"
+			]
+		),
+		.target(
+			name: "App",
+			dependencies: [
+				.product(name: "Vapor", package: "vapor"),
+				.product(name: "NIOCore", package: "swift-nio"),
+				.product(name: "NIOPosix", package: "swift-nio"),
+				.product(name: "HtmlVaporSupport", package: "swift-html-vapor")
+			],
+			swiftSettings: swiftSettings
+		),
+		.testTarget(
+			name: "AppTests",
+			dependencies: [
+				.target(name: "App"),
+				.product(name: "XCTVapor", package: "vapor"),
+			],
+			swiftSettings: swiftSettings
+		)
+	]
 )
 
 var swiftSettings: [SwiftSetting] { [
-  .enableUpcomingFeature("DisableOutwardActorInference"),
-  .enableExperimentalFeature("StrictConcurrency"),
+	.enableUpcomingFeature("DisableOutwardActorInference"),
+	.enableExperimentalFeature("StrictConcurrency"),
 ] }
