e4ad1ac8ddbc034b239af9fa7f94bf941bf24c41
Adrian Sergheev <sergheevdev@icloud.com>
2025-12-17 14:25:35 +0100

    wip

--- Sources/App/Views/layout.swift
+++ Sources/App/Views/layout.swift
@@ -5,7 +5,8 @@ import HtmlVaporSupport
 public func layout(
   title: String,
   content: Node,
-  backButton: Bool = true
+  backButton: Bool = true,
+  usePrismJS: Bool = false,
 ) -> Node {
   @Dependency(\.siteRouter) var router
   let footer = Node.footer(
@@ -20,20 +21,73 @@ public func layout(
     .html(
       .head(
         .title(title),
-        .style(safe: indexCSS),
+        .style(unsafe: style(usePrismJS: usePrismJS)),
         .meta(viewport: .width(.deviceWidth), .initialScale(1)),
         .script(attributes: [
           .defer(true),
           .data("domain", "sergheev.com"),
           .src("https://plausible.sergheev.com/js/script.js")
-        ])
+        ]),
       ),
       .body(
         .main(content),
-        backButton
-        ? footer
-        : []
+        backButton ? footer : [],
+        usePrismJS ? prismJS() : []
       )
     )
+  ]
+}
+
+public func style(usePrismJS: Bool) -> String {
+  var style: String = ""
+  style.append(contentsOf: String(describing: indexCSS))
+  if usePrismJS {
+    style.append(contentsOf: String(describing: syntaxCSS))
+  }
+  return style
+}
+
+// syntax highlighting
+public func prismJS() -> Node {
+  return [
+    .script(attributes: [.src("//cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/prism.min.js")]),
+    .script(attributes: [.src("//cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/plugins/line-highlight/prism-line-highlight.min.js")]),
+    .script(attributes: [.src("//cdnjs.cloudflare.com/ajax/libs/prism/1.28.0/components/prism-swift.min.js")]),
+    .script(safe:
+      #"""
+      Prism.languages.swift['class-name'] = [
+        /\b(_[A-Z]\w*)\b/,
+        Prism.languages.swift['class-name']
+      ];
+      Prism.languages.swift.keyword = [
+        /\b(any|macro|sending)\b/,
+        /\b((iOS|macOS|tvOS|watchOS|visionOS)(|ApplicationExtension)|swift)\b/,
+        Prism.languages.swift.keyword
+      ];
+      Prism.languages.swift.comment.inside = {
+        todo: {
+          pattern: /(TODO:)/
+        }
+      };
+      Prism.languages.insertBefore('swift', 'operator', {
+        'code-fold': {
+          pattern: /…/
+        },
+      });
+      Prism.languages.insertBefore('swift', 'string-literal', {
+        'placeholder': {
+          pattern: /<#.+?#>/,
+          inside: {
+            'placeholder-open': {
+              pattern: /<#/
+            },
+            'placeholder-close': {
+              pattern: /#>/
+            },
+          }
+        },
+      });
+      """#
+           )
   ]
 }
